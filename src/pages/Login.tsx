5c9d69232de9ac3e9b8938bf3516a6353784ee56
Vladyslav <vladisglyadelkin@gmail.com>
2026-04-21 02:19:27 +0200

    Address review comments

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,3 @@
-import LayoutPage from '@/layoutPage/LayoutPage';
 import { Button } from '@components/ui/button';
 import loginPreview from '@assets/images/login-preview.png';
 import LogoIcon from '@assets/icons/logo.svg?react';
@@ -6,11 +5,9 @@ import GoogleIcon from '@assets/icons/google.svg?react';
 import { useState } from 'react';
 import PasswordField from '@components/common/forms/PasswordField';
 import TextField from '@components/common/forms/TextField';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 
 const Login = () => {
-  const [password, setPassword] = useState('');
-
   const [formData, setFormData] = useState({
     email: '',
     password: '',
@@ -24,92 +21,90 @@ const Login = () => {
   };
 
   return (
-    <LayoutPage>
-      <div className="flex items-center justify-center ">
-        <div className="hidden lg:block w-[50vw]">
-          <img
-            src={loginPreview}
-            alt="Login preview"
-            className="w-full h-auto rounded-lg"
-          />
-        </div>
-        <div className="w-[50vw] h-full flex items-center justify-center">
-          <div className="w-[448px] flex flex-col items-center justify-center gap-24">
-            <a href="#" className="flex items-center gap-12">
-              <div className="w-48 h-48 bg-[var(--accent)] rounded-full flex items-center justify-center">
-                <LogoIcon className="w-24 h-24 text-white" />
-              </div>
-              <span className="text-[24px] text-[var(--text-h)] font-bold">
-                Verdora
-              </span>
-            </a>
-            <div className="flex flex-col items-center">
-              <h2 className="my-[8px] text-[36px] leading-[40px] text-[var(--text-h)] font-bold font-outfit">
-                Sign in to Verdora
-              </h2>
-              <p className="text-[var(--text)] text-[16px]">
-                Access your account and orders
-              </p>
+    <div className="flex items-center justify-center ">
+      <div className="hidden lg:block w-[50vw]">
+        <img
+          src={loginPreview}
+          alt="Login preview"
+          className="w-full h-auto rounded-lg"
+        />
+      </div>
+      <div className="w-[50vw] h-full flex items-center justify-center">
+        <div className="w-[448px] flex flex-col items-center justify-center gap-24">
+          <Link to="/" className="flex items-center gap-8">
+            <div className="flex size-40 items-center justify-center rounded-full bg-[var(--accent)]">
+              <LogoIcon className="size-20 text-white" />
             </div>
-            <form className="flex flex-col items-center justify-center gap-24 w-full">
-              <div className="w-full">
-                <TextField
-                  type="text"
-                  label="Email Address"
-                  id="email"
-                  placeholder="your@email.com"
-                  value={formData.email}
-                  onChange={value => handleInputChange('email', value)}
-                />
-              </div>
-              <div className="w-full">
-                <PasswordField
-                  label="Password"
-                  labelRight={
-                    <a
-                      href="#"
-                      className="text-sm text-[var(--accent)] hover:underline"
-                    >
-                      Forgot password?
-                    </a>
-                  }
-                  placeholder="Enter your password"
-                  value={password}
-                  onChange={setPassword}
-                />
-              </div>
-              <Button className="w-full" variant={'active'} type="submit">
-                Sign In
-              </Button>
-            </form>
-            <div className="w-full flex items-center gap-4">
-              <div className="h-px flex-1 bg-zinc-200" />
-              <span className="text-[14px] text-zinc-500 mx-[16px]">
-                Or continue with
-              </span>
-              <div className="h-px flex-1 bg-zinc-200" />
-            </div>
-            <Button
-              variant={'default'}
-              className="w-full cursor-pointer"
-              type="button"
-            >
-              <GoogleIcon className="size-6 w-20 h-20 mr-12 cursor-pointer" />
-              Continue with Google
-            </Button>
-            <p className="text-[16px] text-zinc-500">
-              Don't have an account?{' '}
-              <Link
-                to="/register"
-                className="text-[var(--accent)] hover:underline"
-              >
-                Sign up
-              </Link>
+            <span className="text-[20px] font-bold text-[var(--text-h)]">
+              Verdora
+            </span>
+          </Link>
+          <div className="flex flex-col items-center">
+            <h2 className="my-[8px] text-[36px] leading-[40px] text-[var(--text-h)] font-bold font-outfit">
+              Sign in to Verdora
+            </h2>
+            <p className="text-[var(--text)] text-[16px]">
+              Access your account and orders
             </p>
           </div>
+          <form className="flex flex-col items-center justify-center gap-24 w-full">
+            <div className="w-full">
+              <TextField
+                type="text"
+                label="Email Address"
+                id="email"
+                placeholder="your@email.com"
+                value={formData.email}
+                onChange={value => handleInputChange('email', value)}
+              />
+            </div>
+            <div className="w-full">
+              <PasswordField
+                label="Password"
+                labelRight={
+                  <a
+                    href="#"
+                    className="text-sm text-[var(--accent)] hover:underline"
+                  >
+                    Forgot password?
+                  </a>
+                }
+                placeholder="Enter your password"
+                value={formData.password}
+                onChange={value => handleInputChange('password', value)}
+              />
+            </div>
+            <Button className="w-full" variant={'active'} type="submit">
+              Sign In
+            </Button>
+          </form>
+          <div className="w-full flex items-center gap-4">
+            <div className="h-px flex-1 bg-zinc-200" />
+            <span className="text-[14px] text-zinc-500 mx-[16px]">
+              Or continue with
+            </span>
+            <div className="h-px flex-1 bg-zinc-200" />
+          </div>
+          <Button
+            variant={'default'}
+            className="w-full cursor-pointer"
+            type="button"
+          >
+            <GoogleIcon className="size-6 w-20 h-20 mr-12 cursor-pointer" />
+            Continue with Google
+          </Button>
+          <p className="text-[16px] text-zinc-500">
+            Don't have an account?{' '}
+            <Link
+              to="/register"
+              className="text-[var(--accent)] hover:underline"
+            >
+              Sign up
+            </Link>
+          </p>
         </div>
       </div>
-    </LayoutPage>
+    </div>
   );
 };
 
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,11 +1,10 @@
-import LayoutPage from '@/layoutPage/LayoutPage';
 import { Button } from '@components/ui/button';
 import loginPreview from '@assets/images/login-preview.png';
 import LogoIcon from '@assets/icons/logo.svg?react';
 import { useState } from 'react';
 import PasswordField from '@components/common/forms/PasswordField';
 import TextField from '@components/common/forms/TextField';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 import CheckIcon from '@assets/icons/checkbox.svg?react';
 import { cn } from '@/lib/utils';
 
@@ -25,144 +24,137 @@ const Register = () => {
   };
 
   return (
-    <LayoutPage>
-      <div className="flex items-center justify-center ">
-        <div className="hidden lg:block w-[50vw]">
-          <img
-            src={loginPreview}
-            alt="Login preview"
-            className="w-full h-auto rounded-lg"
-          />
-        </div>
-        <div className="w-[50vw] h-full flex items-center justify-center">
-          <div className="w-[448px] flex flex-col items-center justify-center gap-24">
-            <a href="#" className="flex items-center gap-12">
-              <div className="w-48 h-48 bg-[var(--accent)] rounded-full flex items-center justify-center">
-                <LogoIcon className="w-24 h-24 text-white" />
-              </div>
-              <span className="text-[24px] text-[var(--text-h)] font-bold">
-                Verdora
-              </span>
-            </a>
-            <div className="flex flex-col items-center">
-              <h2 className="my-[8px] text-[36px] leading-[40px] text-[var(--text-h)] font-bold font-outfit">
-                Create Account
-              </h2>
-              <p className="text-[var(--text)] text-[16px] [font-family:var(--font-sans)]">
-                Join Verdora and start your green journey
-              </p>
+    <div className="flex items-center justify-center ">
+      <div className="hidden lg:block w-[50vw]">
+        <img
+          src={loginPreview}
+          alt="Login preview"
+          className="w-full h-auto rounded-lg"
+        />
+      </div>
+      <div className="w-[50vw] h-full flex items-center justify-center">
+        <div className="w-[448px] flex flex-col items-center justify-center gap-24">
+          <Link to="/" className="flex items-center gap-8">
+            <div className="flex size-40 items-center justify-center rounded-full bg-[var(--accent)]">
+              <LogoIcon className="size-20 text-white" />
             </div>
-            <form className="flex flex-col items-center justify-center gap-24 w-full">
-              <div className="w-full">
-                <TextField
-                  type="text"
-                  label="Full Name"
-                  id="fullName"
-                  placeholder="John Doe"
-                  value={formData.fullName}
-                  onChange={value => handleInputChange('fullName', value)}
-                />
-              </div>
-              <div className="w-full">
-                <TextField
-                  type="text"
-                  label="Email Address"
-                  id="email"
-                  placeholder="your@email.com"
-                  value={formData.email}
-                  onChange={value => handleInputChange('email', value)}
-                />
-              </div>
-              <div className="w-full">
-                <TextField
-                  type="tel"
-                  label="Phone Number"
-                  id="phone"
-                  placeholder="+1 (555) 000-0000"
-                  value={formData.phone}
-                  onChange={value => handleInputChange('phone', value)}
-                />
-              </div>
-              <div className="w-full">
-                <PasswordField
-                  label="Password"
-                  placeholder="Enter your password"
-                  value={formData.password}
-                  onChange={value => handleInputChange('password', value)}
-                />
-              </div>
-              <div className="w-full">
-                <PasswordField
-                  label="Confirm Password"
-                  placeholder="Re-enter your password"
-                  value={formData.confirmPassword}
-                  onChange={value =>
-                    handleInputChange('confirmPassword', value)
-                  }
-                />
-              </div>
-              <div className="w-full">
-                <label className="flex items-center gap-4 cursor-pointer">
-                  <input
-                    type="checkbox"
-                    checked={accepted}
-                    onChange={e => setAccepted(e.target.checked)}
-                    className="sr-only"
-                  />
-
-                  <div
-                    className={cn(
-                      'flex size-16 items-center justify-center rounded-[4px] border border-zinc-400 bg-white mr-4 transition',
-                      accepted && 'border-[#2F5BFF] bg-[#2F5BFF]'
-                    )}
-                  >
-                    <CheckIcon
-                      className={cn(
-                        'size-8 text-white opacity-0 transition',
-                        accepted && 'opacity-100'
-                      )}
-                    />
-                  </div>
-
-                  <span className="text-[14px] text-[var(--text)] [font-family:var(--font-sans)]">
-                    I agree to the{' '}
-                    <a
-                      href="/terms"
-                      className="text-[var(--accent)] underline underline-offset-4"
-                    >
-                      Terms and Conditions
-                    </a>
-                  </span>
-                </label>
-              </div>
-              <Button
-                className="w-full"
-                variant={'active'}
-                type="submit"
-                disabled={
-                  !formData.fullName ||
-                  !formData.email ||
-                  !formData.password ||
-                  formData.password !== formData.confirmPassword ||
-                  !accepted
-                }
-              >
-                Create Account
-              </Button>
-            </form>
-            <p className="text-[16px] text-zinc-500 [font-family:var(--font-sans)]">
-              Already have an account?{' '}
-              <Link
-                to="/login"
-                className="text-[var(--accent)] hover:underline"
-              >
-                Sign in
-              </Link>
+            <span className="text-[20px] font-bold text-[var(--text-h)]">
+              Verdora
+            </span>
+          </Link>
+          <div className="flex flex-col items-center">
+            <h2 className="my-[8px] text-[36px] leading-[40px] text-[var(--text-h)] font-bold font-outfit">
+              Create Account
+            </h2>
+            <p className="text-[var(--text)] text-[16px] [font-family:var(--font-sans)]">
+              Join Verdora and start your green journey
             </p>
           </div>
+          <form className="flex flex-col items-center justify-center gap-24 w-full">
+            <div className="w-full">
+              <TextField
+                type="text"
+                label="Full Name"
+                id="fullName"
+                placeholder="John Doe"
+                value={formData.fullName}
+                onChange={value => handleInputChange('fullName', value)}
+              />
+            </div>
+            <div className="w-full">
+              <TextField
+                type="text"
+                label="Email Address"
+                id="email"
+                placeholder="your@email.com"
+                value={formData.email}
+                onChange={value => handleInputChange('email', value)}
+              />
+            </div>
+            <div className="w-full">
+              <TextField
+                type="tel"
+                label="Phone Number"
+                id="phone"
+                placeholder="+1 (555) 000-0000"
+                value={formData.phone}
+                onChange={value => handleInputChange('phone', value)}
+              />
+            </div>
+            <div className="w-full">
+              <PasswordField
+                label="Password"
+                placeholder="Enter your password"
+                value={formData.password}
+                onChange={value => handleInputChange('password', value)}
+              />
+            </div>
+            <div className="w-full">
+              <PasswordField
+                label="Confirm Password"
+                placeholder="Re-enter your password"
+                value={formData.confirmPassword}
+                onChange={value => handleInputChange('confirmPassword', value)}
+              />
+            </div>
+            <div className="w-full">
+              <label className="flex items-center gap-4 cursor-pointer">
+                <input
+                  type="checkbox"
+                  checked={accepted}
+                  onChange={e => setAccepted(e.target.checked)}
+                  className="sr-only"
+                />
+
+                <div
+                  className={cn(
+                    'flex size-16 items-center justify-center rounded-[4px] border border-zinc-400 bg-white mr-4 transition',
+                    accepted && 'border-[#2F5BFF] bg-[#2F5BFF]'
+                  )}
+                >
+                  <CheckIcon
+                    className={cn(
+                      'size-8 text-white opacity-0 transition',
+                      accepted && 'opacity-100'
+                    )}
+                  />
+                </div>
+
+                <span className="text-[14px] text-[var(--text)] [font-family:var(--font-sans)]">
+                  I agree to the{' '}
+                  <a
+                    href="/terms"
+                    className="text-[var(--accent)] underline underline-offset-4"
+                  >
+                    Terms and Conditions
+                  </a>
+                </span>
+              </label>
+            </div>
+            <Button
+              className="w-full"
+              variant={'active'}
+              type="submit"
+              disabled={
+                !formData.fullName ||
+                !formData.email ||
+                !formData.password ||
+                formData.password !== formData.confirmPassword ||
+                !accepted
+              }
+            >
+              Create Account
+            </Button>
+          </form>
+          <p className="text-[16px] text-zinc-500 [font-family:var(--font-sans)]">
+            Already have an account?{' '}
+            <Link to="/login" className="text-[var(--accent)] hover:underline">
+              Sign in
+            </Link>
+          </p>
         </div>
       </div>
-    </LayoutPage>
+    </div>
   );
 };
 
